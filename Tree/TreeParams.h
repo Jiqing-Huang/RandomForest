ee14750ddd71db5fc90256a7a639476f0705dab3
Jiqing-Huang <huangjiqing54@gmail.com>
2018-01-26 20:00:14 +0800

    support generic feature and label types

--- Tree/TreeParams.h
+++ Tree/TreeParams.h
@@ -4,21 +4,28 @@
 #define DECISIONTREE_HYPERPARAMS_H
 
 #include <cstdint>
+#include <boost/variant.hpp>
+#include <boost/variant/apply_visitor.hpp>
+#include <boost/variant/static_visitor.hpp>
+#include <boost/variant/multivisitors.hpp>
 #include "../Dataset/Dataset.h"
+#include "../Util/Maths.h"
 
-class TreeParams {
-
- public:
+struct TreeParams {
 
   // input hyperparamters
-  uint32_t cost_function;
-  uint32_t min_leaf_node;
-  uint32_t min_split_node;
-  uint32_t max_depth;
-  uint32_t num_features_for_split;
-  uint32_t random_state;
+  const uint32_t cost_function;
+  const uint32_t min_leaf_node;
+  const uint32_t min_split_node;
+  const uint32_t max_depth;
+  const uint32_t num_features_for_split;
+  const uint32_t random_state;
 
   // calculated
+
+  uint32_t effective_min_leaf_node;
+  uint32_t effective_min_split_node;
+
   uint32_t max_num_leaf;
   uint32_t max_num_cell;
   uint32_t max_num_node;
@@ -31,10 +38,13 @@ class TreeParams {
              uint32_t random_state):
           cost_function(cost_function), min_leaf_node(min_leaf_node), min_split_node(min_split_node),
           max_depth(max_depth), num_features_for_split(num_features_for_split), random_state(random_state),
-          max_num_leaf(0), max_num_cell(0), max_num_node(0) {};
+          effective_min_leaf_node(0), effective_min_split_node(0), max_num_leaf(0), max_num_cell(0), max_num_node(0) {};
 
-  void GetMaxNums(const Dataset &dataset) {
-    max_num_leaf = dataset.weighted_num_samples / min_leaf_node;
+  void SetMaxNums(const uint32_t wnum_samples,
+                  const double multiplier) {
+    effective_min_leaf_node = static_cast<uint32_t>(0.5 + min_leaf_node * multiplier);
+    effective_min_split_node = static_cast<uint32_t>(0.5 + min_split_node * multiplier);
+    max_num_leaf = wnum_samples / min_leaf_node;
     max_num_cell = max_num_leaf - 1;
     max_num_node = max_num_cell + max_num_leaf;
   }
@@ -44,6 +54,9 @@ class TreeParams {
     max_num_cell = 0;
     max_num_node = 0;
   }
+
+ private:
+
 };
 
 #endif
